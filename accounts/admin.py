6832baca13781e5823b062abbbb7fd334cf17138
hanieh parnoon <hanieh.satir@gmail.com>
2022-02-04 01:06:36 +0330

    Custom admin's accounts

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,42 @@
 from django.contrib import admin
-from .models import CustomUser,Customer,Admin
+from django.contrib.auth.admin import UserAdmin
+from .models import *
+from django.db.models import Q
 
 # Register your models here.
-admin.site.register(CustomUser)
-admin.site.register(Admin)
-admin.site.register(Customer)
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin): 
+    model = CustomUser
+    def save_model(self, request, obj, form, change):
+        obj.set_password(form.cleaned_data["password"])
+        obj.save()
+    
+
+@admin.register(Admin)
+class CustomAdmin(admin.ModelAdmin):
+    model = Admin
+    def get_queryset(self, request):
+        return Admin.objects.filter(is_superuser = True)
+    def save_model(self, request, obj, form, change):
+        obj.set_password(form.cleaned_data["password"])
+        obj.save()    
+
+
+@admin.register(Customer)
+class CustomCustomer(admin.ModelAdmin):
+    model = Customer
+    def get_queryset(self, request):
+        return Customer.objects.filter(Q(is_staff= False )and Q(is_superuser = False))
+        
+    def save_model(self, request, obj, form, change):
+        obj.set_password(form.cleaned_data["password"])
+        obj.save()    
+
+   
+
+
+  
+
+
+
