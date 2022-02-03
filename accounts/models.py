40393f19ede355e59d35d69f882c75ca7971fadb
hanieh parnoon <hanieh.satir@gmail.com>
2022-02-03 22:19:09 +0330

    add Admin,Customer and CustomUser models

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(null = True, blank = True,unique=True)
+    device = models.CharField(max_length=50, null = True, blank = True)
  
+    
+class Admin(CustomUser):
+    class Meta:
+        proxy = True
+
+    def save(self, *args, **kwargs):
+        if not self.id :
+            self.is_superuser = True
+        super(Admin, self).save(*args, **kwargs)    
+
+class Customer(CustomUser):
+
+    class Meta:
+        proxy = True
+
+    def save(self, *args, **kwargs):
+        if not self.id :
+            self.is_superuser = False
+            self.is_staff = False
+        super(Customer, self).save(*args, **kwargs)   
 
 
